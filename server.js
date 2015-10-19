9ab9927c0e05efdb5a6aacc9442e89f2149158e1
Kim, Hirokuni <yangkookkim@gmail.com>
2015-10-20 01:18:02 +0900

    wip

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var path = require('path');
-var path = require('path');
 //var express = require('express');
 //var app     = express();
 
@@ -12,40 +11,55 @@ function write_output(output_file, json) {
     })
 }
 
-function test_release(name) {
-    var url = 'https://github.com/kimh/release_note/releases';
+function github_standard_release(name, url) {
+    var dir = 'data';
+    var output_json = path.join(dir, name + ".json");
+    var releases_json = [];
+
+    if (!fs.existsSync(dir)) {fs.mkdirSync(dir);}
 
     request(url, function(error, response, html){
 	if(!error){
 	    var $ = cheerio.load(html);
 	    var releases = $('.release');
 	    var latest = releases.first();
-	    var output_json = name + ".json";
-	    
-	    latest.filter(function() {
-		var data = $(this);
-    		var version, title, description;
-		var json = { name: name, version: "", title: "", description: "" };
 
-		var version = data.find(".release-meta .css-truncate-target").text().trim()
-		var title   = data.find('.release-body .release-title').text().trim()
-		var desc    = data.find('.release-body .markdown-body').text().trim()
-		
-		console.log("version: %s", version);
-		console.log("title: %s", title);
-		console.log("description: %s", desc);
+	    releases.each(function(i, release) {
+		$(release).filter(function() {
+		    var data = $(this);
+    		    var version, title, description;
+		    var json = { name: name, version: "", title: "", description: "" };
 
-		json.version = version;
-		json.title = title;
-		json.description = desc;
+		    var version = data.find(".release-meta .css-truncate-target").text().trim()
+		    var title   = data.find('.release-body .release-title').text().trim()
+		    var desc    = data.find('.release-body .markdown-body').text().trim()
+		    
+		    console.log("version: %s", version);
+		    console.log("title: %s", title);
+		    console.log("description: %s", desc);
 
-		write_output(path.join('data', output_json), json);
-	    })	
+		    json.version = version;
+		    json.title = title;
+		    json.description = desc;
+
+		    releases_json.push(json)
+		})
+	    });
 	}
-    })
+	
+        write_output(output_json, releases_json);
+    });
 }
 
-test_release("my_release");
+function docker() {
+    github_standard_release("docker", 'https://github.com/docker/docker/releases');
+}
 
+function my_release() {
+    github_standard_release("my_release", 'https://github.com/kimh/release_note/releases');
+}
+
+//docker();
+my_release();
 
 //exports = module.exports = app; 	
