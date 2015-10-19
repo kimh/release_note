7fc0e3b93abd8b78f5e049508958ef0a69d4b7c0
Kim, Hirokuni <yangkookkim@gmail.com>
2015-10-20 00:29:08 +0900

    wip

--- server.js
+++ server.js
@@ -1,42 +1,51 @@
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
+var path = require('path');
+var path = require('path');
 //var express = require('express');
 //var app     = express();
 
-url = 'https://github.com/kimh/release_note/releases';
+function write_output(output_file, json) {
+    fs.writeFile(output_file, JSON.stringify(json, null, 4), function(err){
+	console.log('done');
+    })
+}
 
-request(url, function(error, response, html){
-    if(!error){
-	var $ = cheerio.load(html);
-	var releases = $('.release');
-	var latest = releases.first()
+function test_release(name) {
+    var url = 'https://github.com/kimh/release_note/releases';
 
-	latest.filter(function() {
-	    var data = $(this);
-    	    var version, title, description;
-	    var json = { version: "", title: "", description: "" };
-
-	    var version = data.find(".release-meta .css-truncate-target").text().trim()
-	    var title   = data.find('.release-body .release-title').text().trim()
-	    var desc    = data.find('.release-body .markdown-body').text().trim()
+    request(url, function(error, response, html){
+	if(!error){
+	    var $ = cheerio.load(html);
+	    var releases = $('.release');
+	    var latest = releases.first();
+	    var output_json = name + ".json";
 	    
-	    console.log("version: %s", version);
-	    console.log("title: %s", title);
-	    console.log("description: %s", desc);
+	    latest.filter(function() {
+		var data = $(this);
+    		var version, title, description;
+		var json = { name: name, version: "", title: "", description: "" };
 
-	    json.version = version;
-	    json.title = title;
-	    json.description = desc;
+		var version = data.find(".release-meta .css-truncate-target").text().trim()
+		var title   = data.find('.release-body .release-title').text().trim()
+		var desc    = data.find('.release-body .markdown-body').text().trim()
+		
+		console.log("version: %s", version);
+		console.log("title: %s", title);
+		console.log("description: %s", desc);
 
-	    fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
+		json.version = version;
+		json.title = title;
+		json.description = desc;
 
-		console.log('File successfully written! - Check your project directory for the output.json file');
+		write_output(path.join('data', output_json), json);
+	    })	
+	}
+    })
+}
 
-	    })
+test_release("my_release");
 
-	})	
-    }
-})
 
 //exports = module.exports = app; 	
