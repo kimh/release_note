d476cfa667be0de6d5a00d4cf8c5fe60f644d373
Kim, Hirokuni <yangkookkim@gmail.com>
2015-10-19 23:37:02 +0900

    wip

--- server.js
+++ server.js
@@ -4,42 +4,20 @@ var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
 
-app.get('/scrape', function(req, res){
-	// Let's scrape Anchorman 2
-	url = 'http://www.imdb.com/title/tt1229340/';
+url = 'https://github.com/kimh/release_note/releases/tag/0.0.1';
 
-	request(url, function(error, response, html){
-		if(!error){
-			var $ = cheerio.load(html);
+request(url, function(error, response, html){
+    if(!error){
+	var $ = cheerio.load(html);
 
-			var title, release, rating;
-			var json = { title : "", release : "", rating : ""};
+	var version, title, description;
+	var json = { version: "", title: "", description: "" };
 
-			$('.header').filter(function(){
-		        var data = $(this);
-		        title = data.children().first().text();
-		        release = data.children().last().children().text();
+	var version = $('.release-meta .css-truncate-target').text()
+    }
 
-		        json.title = title;
-		        json.release = release;
-	        })
 
-	        $('.star-box-giga-star').filter(function(){
-	        	var data = $(this);
-	        	rating = data.text();
-
-	        	json.rating = rating;
-	        })
-		}
-
-		fs.writeFile('output.json', JSON.stringify(json, null, 4), function(err){
-        	console.log('File successfully written! - Check your project directory for the output.json file');
-        })
-
-        res.send('Check your console!')
-	})
+    console.log("version: %s", version);
 })
 
-app.listen('8081')
-console.log('Magic happens on port 8081');
 exports = module.exports = app; 	
